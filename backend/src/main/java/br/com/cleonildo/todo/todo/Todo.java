5f7e46be131af3c29ebd95f14b938073a73703ef
Cleonildo Jr <cleu.junior@gmail.com>
2023-02-24 17:50:48 -0300

    feat: Crud Todo

--- backend/src/main/java/br/com/cleonildo/todo/todo/Todo.java
+++ backend/src/main/java/br/com/cleonildo/todo/todo/Todo.java
@@ -4,13 +4,21 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "tb_todo")
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class Todo {
@@ -20,12 +28,15 @@ public class Todo {
     private long id;
 
     @Column
+    @Size(min = 10, max = 255)
     private String description;
 
     @Column
+    @NotNull
     private Boolean done;
 
     @Column
+    @NotNull
     private LocalDateTime created;
 
     @Column
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Finish project 1', false, '2022-02-24 10:00:00', NULL);
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Buy groceries 1', false, '2022-02-23 15:00:00', NULL);
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Go to the gym 1', true, '2022-02-22 08:00:00', '2022-02-22 12:00:00');
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Finish project 2', false, '2022-02-21 10:00:00', NULL);
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Buy groceries 2', false, '2022-02-20 15:00:00', NULL);
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Go to the gym 2', true, '2022-02-19 08:00:00', '2022-02-19 12:00:00');
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Finish project 3', false, '2022-02-18 10:00:00', NULL);
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Buy groceries 3', false, '2022-02-17 15:00:00', NULL);
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Go to the gym 3', true, '2022-02-16 08:00:00', '2022-02-16 12:00:00');
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Finish project 4', false, '2022-02-15 10:00:00', NULL);
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Buy groceries 4', false, '2022-02-14 15:00:00', NULL);
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Go to the gym 4', true, '2022-02-13 08:00:00', '2022-02-13 12:00:00');
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Finish project 5', false, '2022-02-12 10:00:00', NULL);
+INSERT INTO tb_todo (description, done, created, done_date) VALUES ('Buy groceries 5', false, '2022-02-11 15:00:00', NULL);
